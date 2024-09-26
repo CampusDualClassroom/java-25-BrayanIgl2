3d552deaf8cd06f26b02327c71efc1f28055f489
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-27 00:36:30 +0200

    25 ok

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,11 +4,22 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
+        Stack<String> stack = new Stack<>();
 
+        //Se añaden los elementos al Stack
+        stack.add("Smith");
+        stack.add("Montessori");
+        stack.add("Peralta");
+        stack.add("House");
+
+        return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()) {
+            // Imprime el elemento en la parte superior y lo elimina
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
